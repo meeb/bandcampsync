4491061a0ef4dd098e7c075d46ad3f1382b83549
meeb <meeb@meeb.org>
2023-09-29 17:35:56 +1000

    correct PGID typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Set the 'app' user UID and GID from the ENV vars
 PUID="${PUID:-1000}"
-PUID="${PUID:-1000}"
+PGID="${PGID:-1000}"
 groupmod -o -g "$PGID" app
 usermod -o -u "$PUID" app
 echo "Set service UID:GID to ${PUID}:${PGID}"
