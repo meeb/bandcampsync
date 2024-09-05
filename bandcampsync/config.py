6a13ae2273e508c9296531e25936b465e11a6fa1
meeb <meeb@meeb.org>
2024-09-05 16:04:24 +1000

    bump user agent

--- bandcampsync/config.py
+++ bandcampsync/config.py
@@ -1,2 +1,2 @@
 VERSION = '0.4.0'
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0'
