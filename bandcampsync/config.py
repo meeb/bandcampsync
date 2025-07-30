eb34b99285f384025ca4d7cae1f6189a50b44a26
meeb <meeb@meeb.org>
2025-07-30 20:11:37 +1000

    bump to 0.5.3

--- bandcampsync/config.py
+++ bandcampsync/config.py
@@ -1,3 +1,3 @@
-VERSION = '0.5.2'
+VERSION = '0.5.3'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0'
 INTERNAL_USER_AGENT = 'bandcampsync/' + VERSION
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup, find_packages
 
 
-version = '0.5.2'
+version = '0.5.3'
 
 
 with open('README.md', 'rt') as f:
